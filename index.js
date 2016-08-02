c9a98659026f3d728ea8a501c063e86bb12af676
Michael Williams <michael.williams@enspiral.com>
2016-08-02 20:04:56 +1200

    drop support for preserving `this` context

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function Routes (routes) {
 
 function wrap (handler) {
   return function (req, res, params, next) {
-    handler.call(this, extend(req, { params }), res, next)
+    handler(extend(req, { params }), res, next)
   }
 }
